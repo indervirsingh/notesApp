2cd7a25a08b58f1cb193e7d8f305bd02b5fe22ac
Indervir Singh <indervirsingh@protonmail.com>
2022-04-03 05:59:55 -0700

    added code for importing mongoose and express, also some feedback handling for mongoose

---
+++ server.js
@@ -0,0 +1,18 @@
+// Import Express and Mongoose modules
+const express = require('express')
+const mongoose = require('mongoose')
+
+//Create a server-object/instance for the app
+var recipeApp = express()
+
+// Connect the database via mongoose
+mongoose.connect('mongodb://localhost/userStorageDB')
+
+// Feedback handling for mongoose
+mongoose.connection.once("open", () => {
+    console.log("connected to database");
+}).on("error", (error) => {
+    console.log("Failed to connect " + error);
+})
+
+
