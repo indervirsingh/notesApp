f293e6476aafef2710f27b42acd636a24857573f
Indervir Singh <indervirsingh.software@gmail.com>
2025-01-29 12:21:52 -0800

    Refactoring

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ recipeApp.get("/fetch", (req, res) => {
 })
 
 
-// Start server @ http://192.168.1.102:8081/create
-var server = recipeApp.listen(8081, "192.168.1.102", () => {
+// Start server @ http://localhost:8081/create
+var server = recipeApp.listen(8081, "localhost", () => {
     console.log("Server is running!");
 })
