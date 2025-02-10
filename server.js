4bc29d26d88a3cc98068d38cf3eb7a7d79654221
Indervir Singh <indervirsingh.software@gmail.com>
2025-02-10 14:25:04 -0800

    Connected to atlas database, will insert new data later, so far it works but loads no notes

--- server.js
+++ server.js
@@ -1,43 +1,31 @@
-// Import Express and Mongoose modules
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-//Create schema object for server to use, via mongoose 
 const Data = require('./noteSchema')
 
-//Create a server-object/instance for the app
 var recipeApp = express()
 
-// Connect the database via mongoose
-mongoose.connect('mongodb://localhost/userStorageDB')
+const mongoURI = process.env.MONGODB_URI
 
-// Feedback handling for mongoose
+mongoose.connect(mongoURI)
 mongoose.connection.once("open", () => {
     console.log("connected to database")
 }).on("error", (error) => {
     console.log("Failed to connect " + error)
 })
 
-
-
-// ROUTES
-
-// Create a note
 recipeApp.post("/create", (req, res) => {
-
     var note = new Data ({
-
         title: req.get("title"),
         date: req.get("date"),
         photo: req.get("photo"),
         ingredients: req.get("ingredients"),
         recipe: req.get("recipe"),
         summary: req.get("summary"),
-
     })
 
     note.save().then(() => {
-
         if (note.isNew == false) {
             console.log("Saved data")
             res.send("Saved data")
@@ -45,13 +33,9 @@ recipeApp.post("/create", (req, res) => {
             console.log("Failed to save data")
         }
     })
-
 })
 
-// Delete a note
 recipeApp.post("/delete", (req, res) => {
-
-    // Use database object which is connected to mongodb via mongoose to search database and delete the one requested by user
     Data.findOneAndRemove({
         _id: req.get("id")
     }, (err) => {
@@ -60,10 +44,7 @@ recipeApp.post("/delete", (req, res) => {
     res.send("Deleted!")
 })
 
-// Update a note
 recipeApp.post("/update", (req, res) => {
-
-    // (_id, updated Note information, error Message)
     Data.findByIdAndUpdate({
         _id: req.get("id"),
     }, {
@@ -73,25 +54,18 @@ recipeApp.post("/update", (req, res) => {
         ingredients: req.get("ingredients"),
         recipe: req.get("recipe"),
         summary: req.get("summary")
-
     }, (err) => {
         console.log("Failed to update " + err)
     })
-
     res.send("Updated!")
 })
 
-// Fetch all notes
 recipeApp.get("/fetch", (req, res) => {
-
-    // Searches through all of the notes/objects in database then sends them back
     Data.find({}).then((DBitems) => {
         res.send(DBitems)
     })
 })
 
-
-// Start server @ http://localhost:8081/create
 var server = recipeApp.listen(8081, "localhost", () => {
     console.log("Server is running!");
 })
